003864d782ef621cebb2f5541be08915426761f9
Xejb <vladiq-artamonenko@rambler.ru>
2023-09-02 21:16:45 +0300

    routing

--- domen/domen/urls.py
+++ domen/domen/urls.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from django.urls import path
-from automation.views import index, categories
+from django.urls import path, include
+from automation import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('domen/', index),
-    path('cat/', categories)
+    path('', include('automation.urls')),
 ]
