415377b28eea058c691ed1563cae2e6c08d5623b
Xejb <vladiq-artamonenko@rambler.ru>
2023-09-05 16:38:54 +0300

    redirect

--- domen/automation/views.py
+++ domen/automation/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponse, HttpResponseNotFound, Http404
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound, Http404, HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.urls import reverse
 
 
 def index(request):  # HttpRequest
@@ -18,7 +19,9 @@ def categories_by_slug(request, cat_slug):
 
 def archive(request, year):
     if year > 2023:
-        raise Http404()
+        uri = reverse('cats', args=('music', ))
+        return HttpResponseRedirect(uri)
+
     return HttpResponse(f'<h1>Архив по годам</h1><p>{year}</p>')
 
 
