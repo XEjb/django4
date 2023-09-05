2b59fd03331adb6f6779cfa171d61d8ff4349262
Xejb <vladiq-artamonenko@rambler.ru>
2023-09-05 13:40:14 +0300

    custom converters

--- domen/automation/views.py
+++ domen/automation/views.py
@@ -12,3 +12,7 @@ def categories(request, cat_id):
 
 def categories_by_slug(request, cat_slug):
     return HttpResponse(f'<h1>Статьи по категориям</h1><p>slug: {cat_slug}</p>')
+
+
+def archive(request, year):
+    return HttpResponse(f'<h1>Архив по годам</h1><p>{year}</p>')
