96dfed1738b84e8aebab17cb87caa3b3e690f908
Xejb <vladiq-artamonenko@rambler.ru>
2023-09-05 16:03:47 +0300

    request exception handler

--- domen/domen/settings.py
+++ domen/domen/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-w5!gcr40r2t+w1_fo&n=@emz^in-)p-!%a)^=0ewzkie9aae*2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
