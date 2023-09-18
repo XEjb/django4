84846483a8a558ae790147f6ed6bb2cc00f3ddef
Xejb <vladiq-artamonenko@rambler.ru>
2023-09-18 15:29:45 +0300

    template tag url

--- domen/automation/views.py
+++ domen/automation/views.py
@@ -4,7 +4,11 @@ from django.urls import reverse
 from django.template.loader import render_to_string
 from django.template.defaultfilters import slugify
 
-menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
+menu = [{'title': 'О сайте', 'url_name': 'about'},
+        {'title': 'Добавить статью', 'url_name': 'add_page'},
+        {'title': 'Обратная связь', 'url_name': 'contact'},
+        {'title': 'Войти', 'url_name': 'login'}
+        ]
 
 data_db = [
     {'id': 1, 'title': 'Первый', 'content': 'Биография 1', 'is_published': True},
@@ -26,22 +30,20 @@ def about(request):
     return render(request, 'automation/about.html', {'title': 'О сайте'})
 
 
-def categories(request, cat_id):
-    return HttpResponse(f'<h1>Статьи по категориям</h1><p>id: {cat_id}</p>')
+def show_post(request, post_id):
+    return HttpResponse(f'Отображение статьи с id = {post_id}')
 
 
-def categories_by_slug(request, cat_slug):
-    if request.POST:
-        print(request.POST)
-    return HttpResponse(f'<h1>Статьи по категориям</h1><p>slug: {cat_slug}</p>')
+def addpage(request):
+    return HttpResponse('Добавление статьи')
 
 
-def archive(request, year):
-    if year > 2023:
-        uri = reverse('cats', args=('music',))
-        return HttpResponseRedirect(uri)
+def contact(request):
+    return HttpResponse('Обратная связь')
 
-    return HttpResponse(f'<h1>Архив по годам</h1><p>{year}</p>')
+
+def login(request):
+    return HttpResponse('Авторизация')
 
 
 def page_not_found(request, exception):
