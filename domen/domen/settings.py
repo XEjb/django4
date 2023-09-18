b3c739304babd76170c6d9cc953ffd7e8eafc281
Xejb <vladiq-artamonenko@rambler.ru>
2023-09-18 19:53:32 +0300

    connecting static files

--- domen/domen/settings.py
+++ domen/domen/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-w5!gcr40r2t+w1_fo&n=@emz^in-)p-!%a)^=0ewzkie9aae*2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True            # --insecure
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
