3a78263593447663f8e944300795ce218a4f3ab3
Xejb <vladiq-artamonenko@rambler.ru>
2023-10-03 13:07:50 +0300

    grouping records

--- domen/automation/templatetags/automation_tags.py
+++ domen/automation/templatetags/automation_tags.py
@@ -1,4 +1,6 @@
 from django import template
+from django.db.models import Count
+
 import automation.views as views
 from ..models import Category, TagPost
 
@@ -7,10 +9,10 @@ register = template.Library()
 
 @register.inclusion_tag('automation/list_categories.html')
 def show_categories(cat_selected=0):
-    cats = Category.objects.all()
+    cats = Category.objects.annotate(total=Count("posts")).filter(total__gt=0)
     return {'cats': cats, 'cat_selected': cat_selected}
 
 
 @register.inclusion_tag('automation/list_tags.html')
 def show_all_tags(cat_selected=0):
-    return {'tags': TagPost.objects.all()}
+    return {'tags': TagPost.objects.annotate(total=Count("tags")).filter(total__gt=0)}
