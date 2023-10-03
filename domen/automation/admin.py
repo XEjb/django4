038b3eeaa34bb974c039bbb1549998d532d05ecb
Xejb <vladiq-artamonenko@rambler.ru>
2023-10-03 19:40:12 +0300

    custom fields

--- domen/automation/admin.py
+++ domen/automation/admin.py
@@ -1,14 +1,29 @@
-from django.contrib import admin
+from django.contrib import admin, messages
 from .models import Automation, Category
 
 
 @admin.register(Automation)
 class AutomationAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'time_create', 'is_published', 'cat')
-    list_display_links = ('id', 'title')
+    list_display = ('title', 'time_create', 'is_published', 'cat', 'brief_info')
+    list_display_links = ('title',)
     ordering = ['time_create', 'title']
     list_editable = ('is_published',)
     list_per_page = 5
+    actions = ['set_published', 'set_draft']
+
+    @admin.display(description='Краткое описание', ordering='content')
+    def brief_info(self, automation: Automation):
+        return f"Описание {len(automation.content)} символов."
+
+    @admin.action(description='Опубликовать')
+    def set_published(self, request, queryset):
+        count = queryset.update(is_published=Automation.Status.PUBLISHED)
+        self.message_user(request, f"Изменено {count} записей")
+
+    @admin.action(description='Снять с публикации')
+    def set_draft(self, request, queryset):
+        count = queryset.update(is_published=Automation.Status.DRAFT)
+        self.message_user(request, f"{count} записей сняты с публикации!", messages.WARNING)
 
 
 @admin.register(Category)
