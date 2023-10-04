7937f7acab8890c5594ea2630b23b465cbfdb886
Xejb <vladiq-artamonenko@rambler.ru>
2023-10-04 18:17:50 +0300

    search bar and filter panel

--- domen/automation/admin.py
+++ domen/automation/admin.py
@@ -2,6 +2,23 @@ from django.contrib import admin, messages
 from .models import Automation, Category
 
 
+class CssFilter(admin.SimpleListFilter):
+    title = 'Статус ра'
+    parameter_name = 'status'
+
+    def lookups(self, request, model_admin):
+        return [
+            ('prod', 'прод'),
+            ('inf', 'инф'),
+        ]
+
+    def queryset(self, request, queryset):
+        if self.value() == 'prod':
+            return queryset.filter(husband__isnull=False)
+        elif self.value() == 'inf':
+            return queryset.filter(husband__isnull=True)
+
+
 @admin.register(Automation)
 class AutomationAdmin(admin.ModelAdmin):
     list_display = ('title', 'time_create', 'is_published', 'cat', 'brief_info')
@@ -10,6 +27,8 @@ class AutomationAdmin(admin.ModelAdmin):
     list_editable = ('is_published',)
     list_per_page = 5
     actions = ['set_published', 'set_draft']
+    search_fields = ['title__startswith', 'cat__name']
+    list_filter = [CssFilter, 'cat__name', 'is_published']
 
     @admin.display(description='Краткое описание', ordering='content')
     def brief_info(self, automation: Automation):
