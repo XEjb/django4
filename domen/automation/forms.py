d6329b01ef4df3bc6fc4f34ba29e9d7a6ad072ae
Xejb <vladiq-artamonenko@rambler.ru>
2023-10-07 17:46:56 +0300

    forms associated with models

--- domen/automation/forms.py
+++ domen/automation/forms.py
@@ -3,7 +3,7 @@ from django.core.exceptions import ValidationError
 from django.core.validators import MinLengthValidator, MaxLengthValidator
 from django.utils.deconstruct import deconstructible
 
-from .models import Category, Husband
+from .models import Category, Husband, Automation
 
 
 @deconstructible
@@ -19,28 +19,22 @@ class Validator:
             raise ValidationError(self.message, code=self.code)
 
 
-class AddPostForm(forms.Form):
-    title = forms.CharField(max_length=255, min_length=4, label='Заголовок',
-                            widget=forms.TextInput(attrs={'class': 'form-input'}),
-                            # validators=[
-                            #     Validator(),
-                            # ],
-                            error_messages={
-                                'min_length': 'Короткий заголовок'
-                            })
-    slug = forms.SlugField(max_length=255, label='URL',
-                           validators=[
-                               MinLengthValidator(5, message='Минимум 5 символов'),
-                               MaxLengthValidator(100),
-                           ])
-    content = forms.CharField(widget=forms.Textarea(attrs={'cols': 50, 'rows': 5}), required=False, label='Content')
-    is_published = forms.BooleanField(required=False, initial=True, label='status')
+class AddPostForm(forms.ModelForm):
     cat = forms.ModelChoiceField(queryset=Category.objects.all(), empty_label='Категория не выбрана', label='Категории')
     husband = forms.ModelChoiceField(queryset=Husband.objects.all(), required=False, label='husband')
 
-    def clean_title(self):
-        title = self.clean_title['title']
-        ALLOWED_CHARS = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЬЫЪЭЮЯабвгдеёжзиёклмнопрстуфхцчшщьыъэюя0123456789- "
+    class Meta:
+        model = Automation
+        fields = ['title', 'slug', 'content', 'is_published', 'cat', 'husband', 'tags']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-input'}),
+            'content': forms.Textarea(attrs={'cols': 50, 'rows': 5}),
+        }
+        labels = {'slug': 'URL'}
 
-        if not (set(title) <= set(ALLOWED_CHARS)):
-            raise ValidationError('Должны присутствовать только рус символы')
+        def clean_title(self):
+            title = self.clean_title['title']
+            if len(title) > 50:
+                raise ValidationError('Длина превышает 50 символов')
+
+            return title
