b0834b021bbc96473e736abc56bab12ad01c603d
Xejb <vladiq-artamonenko@rambler.ru>
2023-10-09 14:42:54 +0300

    ListView

--- domen/automation/views.py
+++ domen/automation/views.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 from django.template.loader import render_to_string
 from django.template.defaultfilters import slugify
 from django.views import View
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
 
 from .forms import AddPostForm, UploadFileForm
 from .models import Automation, Category, TagPost, UploadFiles
@@ -16,26 +16,38 @@ menu = [{'title': 'О сайте', 'url_name': 'about'},
         ]
 
 
-def index(request):
-    posts = Automation.published.all().select_related('cat')
+# def index(request):
+#     posts = Automation.published.all().select_related('cat')
+#
+#     data = {
+#         'title': 'Главная страница',
+#         'menu': menu,
+#         'posts': posts,
+#         'cat_selected': 0,
+#     }
+#     return render(request, 'automation/index.html', context=data)
 
-    data = {
-        'title': 'Главная страница',
-        'menu': menu,
-        'posts': posts,
-        'cat_selected': 0,
-    }
-    return render(request, 'automation/index.html', context=data)
 
-
-class AutomationHome(TemplateView):
+class AutomationHome(ListView):
+    # model = Automation
     template_name = 'automation/index.html'
+    context_object_name = 'posts'
     extra_context = {
         'title': 'Главная страница',
         'menu': menu,
-        'posts': Automation.published.all().select_related('cat'),
         'cat_selected': 0,
     }
+
+    def get_queryset(self):
+        return Automation.published.all().select_related('cat')
+
+    # template_name = 'automation/index.html'
+    # extra_context = {
+    #     'title': 'Главная страница',
+    #     'menu': menu,
+    #     'posts': Automation.published.all().select_related('cat'),
+    #     'cat_selected': 0,
+    # }
 
     # def get_context_data(self, **kwargs):
     #     context = super().get_context_data(**kwargs)
@@ -77,27 +89,27 @@ def show_post(request, post_slug):
     return render(request, 'automation/post.html', data)
 
 
-def addpage(request):
-    if request.method == 'POST':
-        form = AddPostForm(request.POST, request.FILES)
-        if form.is_valid():
-            # print(form.cleaned_data)
-            # try:
-            #     Automation.objects.create(**form.cleaned_data)
-            #     return redirect('home')
-            # except:
-            #     form.add_error(None, 'Ошибка добавления поста')
-            form.save()
-            return redirect('home')
-    else:
-        form = AddPostForm()
-
-    data = {
-        'menu': menu,
-        'title': 'Добавление статьи',
-        'form': form,
-    }
-    return render(request, 'automation/addpage.html', data)
+# def addpage(request):
+#     if request.method == 'POST':
+#         form = AddPostForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             # print(form.cleaned_data)
+#             # try:
+#             #     Automation.objects.create(**form.cleaned_data)
+#             #     return redirect('home')
+#             # except:
+#             #     form.add_error(None, 'Ошибка добавления поста')
+#             form.save()
+#             return redirect('home')
+#     else:
+#         form = AddPostForm()
+#
+#     data = {
+#         'menu': menu,
+#         'title': 'Добавление статьи',
+#         'form': form,
+#     }
+#     return render(request, 'automation/addpage.html', data)
 
 
 class AddPage(View):
@@ -143,6 +155,23 @@ def show_category(request, cat_slug):
         'cat_selected': category.pk,
     }
     return render(request, 'automation/index.html', context=data)
+
+
+class AutomationCategory(ListView):
+    template_name = 'automation/index.html'
+    context_object_name = 'posts'
+    allow_empty = False
+
+    def get_queryset(self):
+        return Automation.published.filter(cat__slug=self.kwargs['cat_slug']).select_related('cat')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        cat = context['posts'][0].cat
+        context['title'] = 'Категория - ' + cat.name
+        context['menu'] = menu
+        context['cat_selected'] = cat.pk
+        return context
 
 
 def page_not_found(request, exception):
