4dcec7ec41cadb3bd49704214028357984102c49
Xejb <vladiq-artamonenko@rambler.ru>
2023-10-26 16:08:04 +0300

    stub

--- domen/domen/settings.py
+++ domen/domen/settings.py
@@ -105,6 +105,7 @@ DATABASES = {
 
 CACHES = {
     "default": {
+        # "BACKEND": "django.core.cache.backends.dummy.DummyCache",  # заглушка
         "BACKEND": "django.core.cache.backends.redis.RedisCache",
         "LOCATION": "redis://127.0.0.1:6379",
     }
