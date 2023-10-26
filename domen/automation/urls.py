b205024036c348bf8be765c5f0ac97bca9aed3b7
Xejb <vladiq-artamonenko@rambler.ru>
2023-10-26 16:00:05 +0300

    the per-view cache

--- domen/automation/urls.py
+++ domen/automation/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, re_path, register_converter
 from . import views
 from . import converters
+from django.views.decorators.cache import cache_page
 
 register_converter(converters.FourDigitYearConverter, "year4")
 
 urlpatterns = [
-    path('', views.AutomationHome.as_view(), name='home'),
+    path('', cache_page(30)(views.AutomationHome.as_view()), name='home'),
     path('about/', views.about, name='about'),
     path('addpage/', views.AddPage.as_view(), name='add_page'),
     path('contact/', views.ContactFormView.as_view(), name='contact'),
