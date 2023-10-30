f5e888d9d9b39b65b1f76cf9c7f63fd138f29719
Xejb <vladiq-artamonenko@rambler.ru>
2023-10-30 15:52:48 +0300

    adding a site map

--- domen/domen/settings.py
+++ domen/domen/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'debug_toolbar',
     'social_django',
     'captcha',
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
 ]
 
 MIDDLEWARE = [
@@ -187,3 +189,6 @@ SOCIAL_AUTH_PIPELINE = (
     'social_core.pipeline.social_auth.load_extra_data',
     'social_core.pipeline.user.user_details',
 )
+
+
+SITE_ID = 1
