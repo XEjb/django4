ada202b5028c6229ae1763065a134f3aa86617a9
Xejb <vladiq-artamonenko@rambler.ru>
2023-10-30 16:48:45 +0300

    unittest

--- domen/automation/tests.py
+++ domen/automation/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase
+from http import HTTPStatus
 
-# Create your tests here.
+from django.test import TestCase
+from django.urls import reverse
+
+
+class GetPagesTestCase(TestCase):
+    def setUP(self):
+        pass
+
+    def test_mainpage(self):
+        path = reverse('home')
+        response = self.client.get(path)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        # self.assertIn('automation/index.html', response.temlate_name)
+        self.assertTemplateUsed(response, 'automation/index.html')
+        self.assertEqual(response.context_data['title'], "Главная страница")
+
+    def test_redirect_addpage(self):
+        path = reverse('add_page')
+        redirect_uri = reverse('users:login') + '?next=' + path
+        response = self.client.get(path)
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+        self.assertRedirects(response, redirect_uri)
+
+    def tearDown(self):
+        pass
