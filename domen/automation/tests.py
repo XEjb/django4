88b82471cd48576e26802629e9d625d63a61b4d9
Xejb <vladiq-artamonenko@rambler.ru>
2023-11-01 14:57:24 +0300

    using the database in unittest

--- domen/automation/tests.py
+++ domen/automation/tests.py
@@ -3,8 +3,13 @@ from http import HTTPStatus
 from django.test import TestCase
 from django.urls import reverse
 
+from automation.models import Automation
+
 
 class GetPagesTestCase(TestCase):
+    fixtures = ['automation_automation.json', 'automation_category.json',
+                'automation_husband.json', 'automation_tagpost.json']
+
     def setUP(self):
         pass
 
@@ -22,6 +27,26 @@ class GetPagesTestCase(TestCase):
         response = self.client.get(path)
         self.assertEqual(response.status_code, HTTPStatus.FOUND)
         self.assertRedirects(response, redirect_uri)
+
+    def test_data_mainpage(self):
+        w = Automation.published.all().select_related('cat')
+        path = reverse('home')
+        response = self.client.get(path)
+        self.assertQuerySetEqual(response.context_data['posts'], w[:5])
+
+    def test_paginate_mainpage(self):
+        path = reverse('home')
+        page = 2
+        paginate_by = 5
+        response = self.client.get(path + f'?page={page}')
+        w = Automation.published.all().select_related('cat')
+        self.assertQuerysetEqual(response.context_data['posts'], w[(page - 1) * paginate_by:page * paginate_by])
+
+    def test_content_post(self):
+        w = Automation.published.get(pk=1)
+        path = reverse('post', args=[w.slug])
+        response = self.client.get(path)
+        self.assertEqual(w.content, response.context_data['post'].content)
 
     def tearDown(self):
         pass
